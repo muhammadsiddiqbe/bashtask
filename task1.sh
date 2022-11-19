80681d8f77a698226dd898dd7651c682395e0b2e
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2022-11-19 15:58:38 +0500

    update task 1

---
+++ task1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "id,location_id,name,title,department, email" > accounts_new.csv # add column names
+
+while IFS="," read -r id location_id name title email department
+do
+
+  fullnameArr=(${name// / }) # split into array
+  firstname=${fullnameArr[0],,} # get firstname manipulate all to lowercase
+  lastname=${fullnameArr[1],,} # get lastname manipulate all to lowercase
+
+  email=${firstname:0:1}.$lastname@abc.com # genereate email address
+  DEPARTMENT=
+
+  echo "$id,$location_id,${firstname^} ${lastname^},$title,$department$email" >> accounts_new.csv
+
+  # printf '%s\n' $id $location_id $name $title $email $department | paste -sd ',' >> accounts_new.csv
+
+done < <(tail -n +2 accounts.csv)
+
+
+# while read p; do
+#   echo "$p"
+# done < accounts.csv # read file by line
+
+# while IFS=, read -r one two three four; do
+#     case $two in
+#       1) echo "$one,$two,$three,$four" >>B_1.csv;;
+#       2) echo "$one,$two,$three,$four" >>B_2.csv;;
+#     esac
+# done < accounts.csv
