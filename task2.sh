6b9ed33b2ea9ebe0429ae40dc1623b373aea7343
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2022-11-20 19:32:07 +0500

    update

--- task2.sh
+++ task2.sh
@@ -1,4 +1,23 @@
+#!/bin/bash
+
 file="$1"
 
-head -n 1 $file; # get the first line
-tail -n 1 $file # get the last line
+resultsFile=$(cat $file)
+
+header=$(head -n 1 $file) # get the first line
+footer=$(tail -n 1 $file) # get the last line
+
+# IFS="-----------------------------------------------------------------------------------" read -ra result <<< $resultsFile
+# echo "${result[*]}"
+
+# # READ FOOTER VALUES
+# IFS=',' read -ra split <<< "$footer"
+
+# passedCount=$(echo "${split[0]}")
+# failedCount=$(echo "${split[1]}")
+# ratedPercent=$(echo "${split[2]}")
+# spentTime=$(echo "${split[4]}")
+
+# echo "${split[*]}"
+
+echo "$resultsFile" | sed 's/.*- \(.*\)-/\1/'
