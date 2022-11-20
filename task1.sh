f0e5da1a9c6f88c8247061ec77b9b2c269bc66e0
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2022-11-20 17:49:23 +0500

    complete task1

--- task1.sh
+++ task1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "id,location_id,name,title,email" > accounts_new.csv # add column names
+sed -r 's/,([^,"]*|"[^"]*")/|\1/g' accounts.csv >> data.csv # I used this to replace , to | and it and it became easier to parse then. I found many ways to do it this one may not be most accurate but working :) i need to optimize it :)
 
-while IFS="," read -r id location_id name title email
+while IFS="|" read -r id location_id name title email
 do
 
   fullnameArr=(${name// / }) # split into array
@@ -19,4 +20,6 @@ do
 
   echo "$id,$location_id,${firstname^} ${lastname^},$title,$email" >> accounts_new.csv
 
-done < <(tail -n +2 accounts.csv)
+done < <(tail -n +2 data.csv)
+
+# I think all working as it is said in the task
