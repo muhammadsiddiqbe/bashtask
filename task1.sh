9a14812f4272cc43203954777b10b16136da4821
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2022-11-20 12:46:42 +0500

    update

--- task1.sh
+++ task1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "id,location_id,name,title,department, email" > accounts_new.csv # add column names
+echo "id,location_id,name,title,email" > accounts_new.csv # add column names
 
-while IFS="," read -r id location_id name title email department
+while IFS="," read -r id location_id name title email
 do
 
   fullnameArr=(${name// / }) # split into array
@@ -11,29 +11,12 @@ do
 
   email=${firstname:0:1}.$lastname@abc.com # genereate email address
 
-  if grep -q "$email" accounts_new.csv
+  if grep -q "$email" accounts_new.csv # check this email exists
   then
-      echo "Duplicate $email"
-      email=${firstname:0:1}.$lastname${location_id:0:1}@abc.com
-  else
-      email=$email
-      echo "not duplicate $email"
+    echo "Duplicate $email"
+    email=${firstname:0:1}.$lastname$location_id@abc.com # if exists add locatio_id also
   fi
 
-  echo "$id,$location_id,${firstname^} ${lastname^},$title,$department,$email" >> accounts_new.csv
-
-  # printf '%s\n' $id $location_id $name $title $email $department | paste -sd ',' >> accounts_new.csv
+  echo "$id,$location_id,${firstname^} ${lastname^},$title,$email" >> accounts_new.csv
 
 done < <(tail -n +2 accounts.csv)
-
-
-# while read p; do
-#   echo "$p"
-# done < accounts.csv # read file by line
-
-# while IFS=, read -r one two three four; do
-#     case $two in
-#       1) echo "$one,$two,$three,$four" >>B_1.csv;;
-#       2) echo "$one,$two,$three,$four" >>B_2.csv;;
-#     esac
-# done < accounts.csv
