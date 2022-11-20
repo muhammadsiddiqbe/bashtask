d21a0d0e6089ebce2c9d7f757b9c4530ab75f0e3
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2022-11-20 20:37:13 +0500

    update

--- task2.sh
+++ task2.sh
@@ -4,8 +4,20 @@ file="$1"
 
 resultsFile=$(cat $file)
 
+# I divided this file to 3 parts to get values
+
 header=$(head -n 1 $file) # get the first line
+body=$(head -n -2 output.txt | tail -n +3) # tests part
 footer=$(tail -n 1 $file) # get the last line
+
+
+echo "header: $header"
+echo "body: $body"
+echo "footer: $footer"
+
+# 
+
+# echo $footer | sed 's/.* rated as (\([^%]*%\).*/\1/'
 
 # IFS="-----------------------------------------------------------------------------------" read -ra result <<< $resultsFile
 # echo "${result[*]}"
@@ -20,4 +32,4 @@ footer=$(tail -n 1 $file) # get the last line
 
 # echo "${split[*]}"
 
-echo "$resultsFile" | sed 's/.*- \(.*\)-/\1/'
+# echo "$resultsFile" | sed 's/.*- \(.*\)-/\1/'
